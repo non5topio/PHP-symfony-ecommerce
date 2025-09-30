9ec3a76bfa4cfc1b0df483da30dedaa680b43828
Sahil Borhade <sahil.borhade@nonstopio.com>
2025-09-30 11:45:00 +0530

    Updated Test File

--- tests/Utils/HelperTest.php
+++ tests/Utils/HelperTest.php
@@ -8,18 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 class HelperTest extends TestCase
 {
-    public function testMapObjectsFromUserKey(): void
-    {
-        $entities = [];
-        $mappedByIds = [];
-        foreach ($entities as $entity) {
-            $mappedByIds[$entity->getName()] = $entity;
-        }
 
-        $result = Helper::mapObjectsFromUserKey($entities, function (Entity $object) {
-            return $object->getName();
-        });
 
-        $this->assertEquals($mappedByIds, $result);
-    }
+    
 }
